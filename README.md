187f013ae2b2fa9048bf947ed9c573a95182e139
FrazierLei <zongfei.lei@zju.edu.cn>
2021-12-26 17:42:22 +0800

    refactor: using asyncio, aiohttp, pyppeteer

--- README.md
+++ README.md
@@ -2,109 +2,14 @@
 
 ![](./images/logo.png)
 
-基于 `Requests` 和 `FFmpeg`  的 PornHub 视频下载工具，支持多任务并行下载。
+目前 main 分支下的代码使用 `Asyncio` + `Aiohttp` + `Pyppeteer`，原先的旧代码保留在其他分支中。
 
-用于练手的一个小脚本，一共 100 行出头的代码。
+## 为什么需要异步下载
 
-初衷是看妹子，但是 coding 的过程中学到很多东西。
+如果下载电影时网速能够拉满的话，一个一个下和一个下完再下另一个并没有什么区别，但是很多时候因为资源服务器设在境外，下载的网速是受到很大限制的。比如你的最大下载速度是 10M/s，但是下载单个资源的最大速度是 1M/s，这样的情况下，同时下载 10 个电影能够最大效率利用你的带宽。
 
+Asyncio 最操蛋的地方是他的 API 在每一个 Python 版本（3.4/3.5/3.7/3.10）中都有不小的变化，这就导致在各种博客、教程里你能看到各种各样的 asyncio 代码。
 
+`Pyppeteer` 最操蛋的地方是他是 `Puppeteer` 的 Python 版本，他的选择器极其难用，不过也可能是我太笨了。所以在解析这部分我是用同步的方法去做的，这必然对整个程序的速度有些影响，但是也不大，毕竟大部分时间我们都在发请求，然后把响应写入硬盘。
 
-## 基于 Selenium 的下载方法
-
-一开始的版本，用到了 `Selenium`，参见 [Selenium 分支](https://github.com/FrazierLei/PornHub-Downloader/tree/selenium)
-
-经测试，main 分支（当前分支）下的方法下载更加稳定，速度也更快，但是 `FFmpeg` 的下载/合并比较烧 CPU，风扇转的飞起，所以还是放在服务器上运行比较安全。
-
-
-
-## 更简单无脑的方法
-
-如果只是临时用一下，完全不用像这样大费周章，可以用 IDM 这样现成的插件，或者更厉害的 CoCoCut。
-
-<img src="./images/cococut.png" alt="image-20210727114139974" style="zoom:50%;" />
-
-具体使用方法可以参考：[在线视频抓取神器：COCOCUT——可见皆可得](https://feifeizaici.xyz/posts/cococut/)
-
-
-
-## 为什么要下载下来？
-
-因为如果只在线看，收藏夹里的作品经常会莫名其妙失踪，比如：
-
-![hongkongdoll](./images/hongkongdoll.png)
-
-
-
-## 环境需求
-
-这些依赖在 MacOS 和 Linux 下安装非常方便，基本都是一行代码就安装好了，Windows 下会稍微麻烦一点，不过也就多废几分钟功夫。
-
-- Python 3.6+
-- requests: 用于下载视频
-- bs4: 用于解析 HTML
-- FFmpeg 和 ffmpy3: 用于下载并合并视频片段
-- Node.js : 用于运行那一小段 JS 代码
-- 代理软件，我用的是 [ClashX](https://github.com/yichengchen/clashX/releases)
-
-脚本中设置了代理软件用到的通信端口：
-
-```python
-os.environ["http_proxy"] = "http://127.0.0.1:7890"
-os.environ["https_proxy"] = "http://127.0.0.1:7890" 
-```
-
-也可以在执行 Python 脚本的终端中进行设置：
-
-```shell
-export http_proxy=http://127.0.0.1:7890
-export https_proxy=http://127.0.0.1:7890
-```
-
-二者的效果是一样的。
-
-
-
-## 原理
-
-P站视频的信息包含在视频页面 HTML 中定义的一个 `flashvars` 开头的变量中：
-
-<img src="./images/flashvars.png" style="zoom:50%;" />
-
-下载链接包含在这个 `videoUrl`中：
-
-![](./images/videoUrl.png)
-
-
-
-如果不利用 `Selenium`，无法得到真实的 mp4 文件的下载地址，但是可以得到包含众多 ts 格式的视频片段地址的 m3u8 文件。这时我们只需要用 `FFmpeg` 把这些 ts 文件下载下来，并合并为 mp4 文件即可。
-
-
-
-## 使用方法
-
-1. 在环境中安装包括 `FFmpeg` 在内的众多依赖
-
-2. 运行脚本
-
-   ```shell
-   $ python pornhub_downloader.py 'https://cn.pornhub.com/view_video.php?viewkey=xxxxxxxxxx' -s './学习资料' -n 2
-   ```
-
-   - url: 指定你感兴趣的学习资料页面或者指定 model 的主页
-   - -s: 视频保存的路径，若省略，则在当前路径下的 Download 文件夹（若不存在则自动新建）
-   - -n: 同时下载的进程数，默认为1
-
-
-
-## 运行效果
-
-支持断点续传，如果连接断开，重新运行即可。
-
-![](./images/run2.png)
-
-## TODO
-
-- [x] 去掉对 `Selenium` 的依赖
-- [x] 支持多任务并行下载
-- [ ] GUI
+![image-20211226173811160](./images/run.png)
--- pornhub_downloader.py
+++ pornhub_downloader.py
@@ -1,94 +1,94 @@
 import os
 import re
 import json
+from tqdm import tqdm
 import argparse
 import requests
+import asyncio
+import aiohttp
+from pyppeteer import launch
 from bs4 import BeautifulSoup
-import ffmpy3
-from multiprocessing.dummy import Pool as ThreadPool
 
 
-def ffmpeg_downloader(url, save_path):
-    """
-    用 ffmpeg 下载并保存
-    :param url: m3u8 文件的地址
-    :param save_path: 视频保存的位置
-    :return: None
-    """
-    ffmpy3.FFmpeg(inputs={url: None}, outputs={save_path: None}).run()
+async def fetch(session, url, dst, pbar=None, headers=None):
+    try:
+        if headers:
+            async with session.get(url, headers=headers) as resp:
+                with(open(dst, 'ab')) as f:
+                    while True:
+                        chunk = await resp.content.read(1024)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+                        pbar.update(1024)
+                pbar.close()
+        else:
+            async with session.get(url) as resp:
+                return resp
+    except Exception as e:
+        print(e)
 
 
-def pornhub_parser(sess, url):
-    """
-    解析得到指定 url 的视频对应的 m3u8 文件的地址
-    :param sess: 会话
-    :param url: 视频或者 model 主页的 url
-    :return: (tuple) (m3u8 文件的地址, 视频文件名)
-    """
-    resp = sess.get(url)
-    bs = BeautifulSoup(resp.text, 'html.parser')
+async def download_from_url(url, dst):
+    async with aiohttp.ClientSession() as session:
+        resp = await fetch(session, url, dst)
+        file_size = int(resp.headers['content-length'])
+        if os.path.exists(dst):
+            first_byte = os.path.getsize(dst)
+        else:
+            first_byte = 0
+        if first_byte >= file_size:
+            return file_size
+        header = {"Range": f"bytes={first_byte}-{file_size}"}
+        pbar = tqdm(
+            total=file_size, initial=first_byte,
+            unit='B', unit_scale=True, desc=dst.rsplit('/', 1)[1])
+        await fetch(session, url, dst, pbar=pbar, headers=header)
 
-    # 解析得到视频名称
-    video_name = bs.find('span', class_="inlineFree").text
-    script = bs.find('div', class_='original mainPlayerDiv').find('script').string
-    script = script.strip('\n').strip('\t')
 
-    # 用正则表达式提取 flashvars 变量名
-    var_name = re.findall('var flashvars_(.*) =', script)[0]
+async def get_single_video(save_path, url, sem):
+    async with sem:
+        page = await browser.newPage()
+        await page.goto(url)
+        bs = BeautifulSoup(await page.content(), 'html.parser')
 
-    # 执行这段 JS 代码
-    js = f"""
-    var playerObjList = {{}}
-    {script}
-    var num = flashvars_{var_name}['mediaDefinitions'].length - 1
-    while (flashvars_{var_name}['mediaDefinitions'][num]['format'] != "mp4")
-    {{
-        num -= 1
-    }}
-    console.log(flashvars_{var_name}['mediaDefinitions'][num]['videoUrl'])
-    """
+        video_name = bs.find('span', class_="inlineFree").text  # 解析得到视频名称
+        script = bs.find('div', class_='original mainPlayerDiv').find('script').string
+        script = script.strip()
+        var_name = re.findall('var flashvars_(.*) =', script)[0]   # 用正则表达式提取 flashvars 变量名
 
-    # 将这段 JS 代码保存在本地，然后用 node 执行
-    with open('pornhub.js', 'w') as f:
-        f.write(js)
+        js = f"""
+        () => {{
+        var playerObjList = {{}}
+        {script}
+        var num = flashvars_{var_name}['mediaDefinitions'].length - 1
+        while (flashvars_{var_name}['mediaDefinitions'][num]['format'] != "mp4")
+        {{
+            num -= 1
+        }}
+        return flashvars_{var_name}['mediaDefinitions'][num]['videoUrl']
+        }}
+        """
+        video_urls = await page.evaluate(js)   # 执行这段JS代码
+        await page.goto(video_urls)
+        bs = BeautifulSoup(await page.content(), 'html.parser')
+        data = json.loads(bs.text)
+        download_url = data[-1]['videoUrl']  # 选择最高清的版本
+        await download_from_url(download_url, f'{save_path}/{video_name}.mp4')
 
-    video_urls = os.popen('node pornhub.js').read()
-    resp = sess.get(video_urls)
-    bs = BeautifulSoup(resp.text, 'html.parser')
-    data = json.loads(bs.text)
 
-    # 选择最高清的版本
-    download_url = data[0]['videoUrl']
-
-    # 删除 JS 文件
-    os.remove('pornhub.js')
-
-    # 下载
-    print(f"分辨率{data[0]['quality']}P的 m3u8 文件下载地址为：{download_url}")
-    # os.popen(f"ffmpeg -i '{download_url}'  -c copy '{filename}'")
-    return download_url, video_name + '.mp4'
-    
-
-if __name__ == '__main__':
-    # 设置代理变量
-    os.environ["http_proxy"] = "http://127.0.0.1:7890"
-    os.environ["https_proxy"] = "http://127.0.0.1:7890" 
-
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", type=str, help="The video's website url")
     parser.add_argument("-s", "--save_path", help="The save path on your PC", default='./Downloads')
     parser.add_argument("-n", "--num_proc", type=int, help="The number of processes", default='1')
     args = parser.parse_args()
 
-    # 创建 Download 文件夹📁
-    os.makedirs(args.save_path, exist_ok=True)
+    global browser
+    browser = await launch()
+    sem = asyncio.Semaphore(5)
 
-    # 初始化 session
-    sess = requests.Session()
-
-    # 下载该 model 的全部视频
-    if 'model' in args.url:
-        # 如果主页连接没有包含 videos，就加上
+    if 'model' in args.url:   # 下载该 model 的全部视频
         if args.url.endswith('videos'):
             url = args.url
         else:
@@ -97,28 +97,18 @@ if __name__ == '__main__':
         resp = requests.get(url)
         bs = BeautifulSoup(resp.text, 'html.parser')
         name = bs.find('h1', itemprop="name").text.strip()
-        video_urls = [a['href'] for a in bs.find('ul', id='mostRecentVideosSection').find_all('a')]
-        video_urls = sorted(set(video_urls), key=video_urls.index)
-        print(f'开始下载 {name} 的视频，共 {len(video_urls)} 个。')
-        
-        model_urls = []
-        for i, url in enumerate(video_urls):
-            print(f'{i + 1}.', end=' ')
-            url = 'https://cn.pornhub.com' + url
-            save_path = os.path.join(args.save_path, name)
-            os.makedirs(save_path, exist_ok=True)
-            download_url, video_name = pornhub_parser(sess, url)
-            save_path = os.path.join(args.save_path, name, video_name) 
-            model_urls.append([download_url, save_path])
+        urls = ['https://cn.pornhub.com' + a['href'] for a in bs.find('ul', id='mostRecentVideosSection').find_all('a')]
+        urls = sorted(set(urls), key=urls.index)
+        print(f'开始下载 {name} 的视频，共 {len(urls)} 个。')
+        save_path = os.path.join(args.save_path, name)
+        os.makedirs(save_path, exist_ok=True)
 
-        # 开 n 个线程池
-        pool = ThreadPool(args.num_proc)
-        results = pool.starmap(ffmpeg_downloader, model_urls)
-        pool.close()
-        pool.join()
-    
-    # 下载单个视频
-    else:
-        download_url, video_name = pornhub_parser(sess, args.url)
-        save_path = os.path.join(args.save_path, video_name) 
-        ffmpeg_downloader(download_url, save_path)
+        tasks = [asyncio.create_task(get_single_video(save_path, url, sem)) for url in urls]
+        await asyncio.wait(tasks)
+    else:  # 下载单个视频
+        await get_single_video(args.save_path, args.url, sem)
+    await browser.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
